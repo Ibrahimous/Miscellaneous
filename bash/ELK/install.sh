aae48be5414c0479fca4388c9738aeea7acb89c7
charlou <charlou@pc-charlou>
2015-01-08 07:39:09 +0100

    Added some install helpers for ELK

---
+++ bash/ELK/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#### Elasticsearch ####
+
+#Download and install the Public Signing Key
+wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+
+#Add the following to your /etc/apt/sources.list to enable the repository
+sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
+
+#Run apt-get update and the repository is ready for use. You can install it with :
+sudo apt-get update && sudo apt-get install elasticsearch logstash
+
+#Configure Elasticsearch to automatically start during bootup :
+#sudo update-rc.d elasticsearch defaults 95 10
+
+#### Logstash ####
+
+#curl -O https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
+#tar zxvf logstash-1.4.2.tar.gz
+#cd logstash-1.4.2
+
+#Démarrer le service
+sudo service elasticsearch start
+
---
+++ bash/ELK/logstash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -O https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
+tar zxvf logstash-1.4.2.tar.gz
+cd logstash-1.4.2
