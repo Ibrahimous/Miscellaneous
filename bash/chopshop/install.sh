049e7ae5b2506d42820ddeb6d3e2de7ec2d38a6c
charlou <charlou@pc-charlou>
2015-01-25 14:51:02 +0100

    modified the installation script

--- bash/chopshop/install.sh
+++ bash/chopshop/install.sh
@@ -4,51 +4,30 @@ sudo apt-get update && sudo aptitude upgrade -ry && sudo aptitude install -yq py
 
 git clone https://github.com/MITRECND/chopshop.git && cd chopshop
 
-sudo pip install pymongo M2Crypto pycrypto dnslib && \
-sudo apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+sudo pip install pymongo M2Crypto pycrypto dnslib
+sudo apt-get clean && sudo rm -r /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Install pynids and remove install dir after to conserve space
-cd /tmp/ && \
-git clone --recursive git://github.com/MITRECND/pynids && \
-cd pynids && \
-python setup.py build && \
-python setup.py install && \
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+cd /tmp/ && git clone --recursive git://github.com/MITRECND/pynids
+cd pynids && python setup.py build && sudo python setup.py install && sudo rm -r /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Install htpy and remove install dir after to conserve space
-cd /tmp/ && \
-git clone --recursive git://github.com/MITRECND/htpy && \
-cd htpy && \
-python setup.py build && \
-python setup.py install && \
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+cd /tmp/ && git clone --recursive git://github.com/MITRECND/htpy
+cd htpy && python setup.py build && sudo python setup.py install && sudo rm -r /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Install yaraprocessor and remove install dir after to conserve space
-cd /tmp/ && \
-git clone git://github.com/MITRECND/yaraprocessor.git && \
-cd yaraprocessor && \
-python setup.py install && \
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+cd /tmp/ && git clone git://github.com/MITRECND/yaraprocessor.git
+cd yaraprocessor && sudo python setup.py install && sudo rm -r /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Install pylibemu and remove install dir after to conserve space
-cd /tmp/ && \
-git clone git://git.carnivore.it/libemu.git && \
-cd libemu && autoreconf -v -i && \
-./configure --enable-python-bindings --prefix=/opt/libemu && \
-make install && \
-echo "/opt/libemu/lib/" >> /etc/ld.so.conf.d/libemu.conf && \
-ldconfig && \
-git clone git://github.com/buffer/pylibemu.git && \
-cd pylibemu && \
-echo /opt/libemu/lib > /etc/ld.so.conf.d/pylibemu.conf && \
-python setup.py build && \
-python setup.py install && \
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+cd /tmp/ && git clone git://git.carnivore.it/libemu.git
+cd libemu && autoreconf -v -i && ./configure --enable-python-bindings --prefix=/opt/libemu && make && sudo make install
+echo "/opt/libemu/lib/" | sudo tee /etc/ld.so.conf.d/libemu.conf
+sudo ldconfig
+git clone git://github.com/buffer/pylibemu.git && cd pylibemu
+echo /opt/libemu/lib | sudo tee /etc/ld.so.conf.d/pylibemu.conf
+python setup.py build && sudo python setup.py install && sudo rm -r /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Install ChopShop and remove install dir after to conserve space
-cd /tmp/ && \
-git clone --recursive git://github.com/MITRECND/chopshop && \
-cd chopshop && \
-make && \
-make install && \
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+cd /tmp/ && git clone --recursive git://github.com/MITRECND/chopshop
+cd chopshop && make && sudo make install
