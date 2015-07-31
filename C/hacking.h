122b61e69297cf222b2ad7fb3cbb9ae2df323858
charlou <charlou@debian>
2015-07-31 15:17:46 +0200

    Added some android stuff

--- C/hacking.h
+++ C/hacking.h
@@ -1,3 +1,6 @@
+#ifndef HACKING_H
+#define HACKING_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,15 +34,17 @@ void dump(const unsigned char *data_buffer, const unsigned int length) {
 		if(((i%16)==15) || (i==length-1)) {
 			for(j=0; j < 15-(i%16); j++)
 				printf("   ");
-				printf("| ");
-				for(j=(i-(i%16)); j <= i; j++) {  // display printable bytes from line
-					byte = data_buffer[j];
-					if((byte > 31) && (byte < 127)) // outside printable char range
-						printf("%c", byte);
-					else
-						printf(".");
-				}
+			printf("| ");
+			for(j=(i-(i%16)); j <= i; j++) {  // display printable bytes from line
+				byte = data_buffer[j];
+				if((byte > 31) && (byte < 127)) // outside printable char range
+					printf("%c", byte);
+				else
+					printf(".");
+			}
 			printf("\n"); // end of the dump line (each line 16 bytes)
 		} // end if
 	} // end for
 }
+
+#endif
