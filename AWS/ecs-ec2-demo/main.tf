88491a3f88a46a2c5ea0d19ea315929225ea8781
Ibrahimous <lion_vnr@hotmail.fr>
2024-05-16 21:36:59 +0200

    created tasks definition

--- AWS/ecs-ec2-demo/main.tf
+++ AWS/ecs-ec2-demo/main.tf
@@ -66,3 +66,268 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+# --- ECS cluster ---
+
+resource "aws_ecs_cluster" "main" {
+  name = "demo-ecs"
+}
+
+/* AWS can automatically create these EC2 instances and scaling them based on load, but for this we need to define Launch Template & Autoscaling Group.
+To implement this we need to create such resources:
+IAM Role for ECS Node
+Security Group (SG) for ECS Node
+Launch Template (describes EC2 instance)
+Autoscaling Group (AG)
+Capacity Provider to connect ECS Cluster & Autoscaling Group */
+
+# --- ECS Node role ---
+
+data "aws_iam_policy_document" "ecs_node_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_node_role" {
+  name_prefix        = "demo-ecs-node-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_node_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_node_role_policy" {
+  role       = aws_iam_role.ecs_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_node" {
+  name_prefix = "demo-ecs-node-profile"
+  path        = "/ecs/instance/"
+  role        = aws_iam_role.ecs_node_role.name
+}
+
+# --- ECS SG ---
+
+resource "aws_security_group" "ecs_node_sg" {
+  name_prefix = "demo-ecs-node-sg"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --- ECS Launch Template ---
+
+data "aws_ssm_parameter" "ecs_node_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+resource "aws_launch_template" "ecs_ec2" {
+  name_prefix            = "demo-ecs-ec2-"
+  image_id               = data.aws_ssm_parameter.ecs_node_ami.value
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ecs_node_sg.id]
+
+  iam_instance_profile { arn = aws_iam_instance_profile.ecs_node.arn }
+  monitoring { enabled = true }
+
+  user_data = base64encode(<<-EOF
+      #!/bin/bash
+      echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config;
+    EOF
+  )
+}
+
+# --- ECS ASG ---
+
+resource "aws_autoscaling_group" "ecs" {
+  name_prefix               = "demo-ecs-asg"
+  vpc_zone_identifier       = aws_subnet.public[*].id
+  min_size                  = 2
+  max_size                  = 8
+  health_check_grace_period = 0
+  health_check_type         = "EC2"
+  protect_from_scale_in     = false
+
+  launch_template {
+    id      = aws_launch_template.ecs_ec2.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "demo-ecs-cluster"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = ""
+    propagate_at_launch = true
+  }
+}
+
+#Capacity providers essentially abstract the underlying compute resources from the scaling logic. 
+resource "aws_ecs_capacity_provider" "main" {
+  name = "demo-ecs-ec2"
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
+    managed_termination_protection = "DISABLED"
+    managed_scaling {
+      maximum_scaling_step_size = 2
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name       = aws_ecs_cluster.main.name
+  capacity_providers = [aws_ecs_capacity_provider.main.name]
+
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.main.name
+    base              = 1
+    weight            = 100
+  }
+}
+
+# --- ECR ---
+
+resource "aws_ecr_repository" "app" {
+  name                 = "demo-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "demo_app_repo_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+# --- ECS Task Role ---
+
+data "aws_iam_policy_document" "ecs_task_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name_prefix        = "demo-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+}
+
+resource "aws_iam_role" "ecs_exec_role" {
+  name_prefix        = "demo-ecs-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# --- Cloud Watch Logs ---
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/ecs/demo"
+  retention_in_days = 14
+}
+
+# --- ECS Task Definition ---
+resource "aws_ecs_task_definition" "app" {
+  family             = "demo-app"
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+  execution_role_arn = aws_iam_role.ecs_exec_role.arn
+  network_mode       = "awsvpc"
+  cpu                = 256
+  memory             = 256
+
+  container_definitions = jsonencode([{
+    name         = "app",
+    image        = "${aws_ecr_repository.app.repository_url}",
+    essential    = true,
+    portMappings = [{ containerPort = 80, hostPort = 80 }],
+
+    environment = [{ name = "example", value = "example" }]
+
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        "awslogs-region"        = "eu-west-3",
+        "awslogs-group"         = aws_cloudwatch_log_group.ecs.name,
+        "awslogs-stream-prefix" = "app"
+      }
+    }
+  }])
+}
+
+# --- ECS Service ---
+
+resource "aws_security_group" "ecs_task" {
+  name_prefix = "ecs-task-sg-"
+  description = "Allow all traffic within the VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#WAAT?
+resource "aws_ecs_service" "app" {
+  name            = "app"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 2
+
+  network_configuration {
+    security_groups = [aws_security_group.ecs_task.id]
+    subnets         = aws_subnet.public[*].id
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.main.name
+    base              = 1
+    weight            = 100
+  }
+
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
